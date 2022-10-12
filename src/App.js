3a3c3c5bcde076b062bbf6f8992a62f572cdb63d
PhanBaoThanh <thanhninjahero@gmail.com>
2022-10-12 16:37:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useState} from 'react'
 
 function App() {
+  const [id,setId] = useState(-1)
+  const data = [
+    {
+      id: 0,
+      question: 'Do You Accept All Major Credit Cards?',
+      answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.'
+    },
+    {
+      id: 1,
+      question: 'Do You Suppport Local Farmers?',
+      answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.'
+    },
+    {
+      id: 2,
+      question: 'Do You Use Organic Ingredients?',
+      answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.'
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='box'>
+        <div className='header'>General Questions</div>
+        <div className='questions'>
+        {
+          data.map(item => (
+            <div className='question'>
+              <div className='question__header'>
+                <div className='question__header__name'>{item.question}</div>
+                <div className='question__header__icon' onClick={() => item.id === id ? setId(-1) : setId(item.id)}>
+                  <p>{item.id === id ? '-' : '+'}</p>
+                </div>
+              </div>
+
+              {
+                item.id === id ? (
+                  <div className='answer'>
+                    <p>{item.question}</p>
+                  </div>
+                ) : (
+                  <></>
+                )
+              }
+              
+            </div>
+          ))
+        }
+          
+        </div>
+      </div>
     </div>
   );
 }
